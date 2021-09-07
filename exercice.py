6f025e7224a4a6423a6536ac25ea4540485b22a1
prionkari <prionkari123@gmail.com>
2021-09-07 17:34:25 -0400

    Exercice fini

--- exercice.py
+++ exercice.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 def majuscule(mot):
     # TODO completer la fonction ici
-    return mot
+    nouveau_mot = ""
+    for lettre in mot:
+       lettre_maj = chr(ord(lettre) - 32)
+       nouveau_mot = nouveau_mot + lettre_maj
+    return nouveau_mot
 
 
 if __name__ == '__main__':
